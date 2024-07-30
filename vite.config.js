cb1263281c3bada84920e62b99964559d37faae5
Parikhit <parikhitritzz@gmail.com>
2024-07-30 16:33:03 +0530

    Resolving hosting issues

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-
-    server: { historyApiFallback: true },
+    base: '/', // Ensure this is set to '/' if your site is hosted at the root of your domain
+    build: {
+        outDir: 'dist',
+    },
 });
